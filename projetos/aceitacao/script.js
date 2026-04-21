41f91a1c89de90583629b001b85f8c120373894e
LEONARDO-DATIOR <leonardowederveiga@gmail.com>
2026-04-21 19:39:46 -0300

    Adicionando logica JS para posicionar o botao não

--- projetos/aceitacao/script.js
+++ projetos/aceitacao/script.js
@@ -1,2 +1,13 @@
 var botaoSim = document.getElementById("botao-sim")
-console.log(botaoSim)
+var botaoNao = document.getElementById("botao-nao")
+
+
+botaoNao.addEventListener('mouseenter', () => {
+    const posicaoHorizontalAleatoria = Math.floor(Math.random() * (window.innerWidth - 30));   
+    const posicaoVerticalAleatoria = Math.floor(Math.random() * (window.innerHeight - 30));   
+    botaoNao.style.position = "absolute";
+    botaoNao.style.left = posicaoHorizontalAleatoria+"px";
+    botaoNao.style.top = posicaoVerticalAleatoria+"px";
+    console.log(botaoNao.style.left)
+})
+
